f305bccd617ecaf3dfc5130923e6558e19439fd1
Ahmed Anter <ahmed18anter@gmail.com>
2026-08-01 04:34:14 +0300

    Delete apps folder to prevent admin build errors during Vercel deployment

--- apps/api/src/common/decorators/current-user.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-export const CurrentUser = createParamDecorator((data: string | undefined, ctx: ExecutionContext) => {
-  const user = ctx.switchToHttp().getRequest().user;
-  return data ? user?.[data] : user;
-});
--- apps/api/src/modules/auth/password.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcryptjs';
-
-@Injectable()
-export class PasswordService {
-  async hash(pw: string) { return bcrypt.hash(pw, 10); }
-  async verify(pw: string, hash: string) { return bcrypt.compare(pw, hash); }
-}
